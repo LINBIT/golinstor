b32ca13cb4706e25c6d199a1f1930815207d79a5
Hayley Swimelar <hayley@linbit.com>
2019-04-15 10:07:55 -0700

    add basic resource def creation test

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/LINBIT/golinstor
 go 1.12
 
 require (
+	github.com/google/go-querystring v1.0.0
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/onsi/ginkgo v1.8.0
+	github.com/onsi/gomega v1.5.0
 	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.3.0
 	github.com/stretchr/testify v1.3.0 // indirect
---
+++ golinstor_suite_test.go
@@ -0,0 +1,51 @@
+package linstor_test
+
+import (
+	"context"
+	"testing"
+
+	lapi "github.com/LINBIT/golinstor/client"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestGolinstor(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Golinstor Suite")
+}
+
+var testCTX = context.Background()
+var namePrefix = "e2e-"
+
+var _ = Describe("Resource Creation", func() {
+
+	client, err := lapi.NewClient()
+	if err != nil {
+		panic(err)
+	}
+
+	Describe("Creating a resource definition", func() {
+		Context("when an resource definition is created with a valid name", func() {
+
+			defName := namePrefix + "simpleResDef"
+			err := client.ResourceDefinitions.Create(testCTX, lapi.ResourceDefinition{Name: defName})
+			It("should not error", func() {
+				Ω(err).ShouldNot(HaveOccurred())
+			})
+
+			It("should have the requested name", func() {
+				By("getting the resource definition")
+				resDef, err := client.ResourceDefinitions.Get(testCTX, defName)
+				Ω(err).ShouldNot(HaveOccurred())
+
+				Ω(resDef.Name).Should(Equal(defName))
+			})
+
+			It("should clean up", func() {
+				By("deleteing the resource definition")
+				Ω(client.ResourceDefinitions.Delete(testCTX, defName)).Should(Succeed())
+			})
+		})
+	})
+})
