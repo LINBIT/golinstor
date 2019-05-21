3ecb700073b7a52ca99b68bd7dedc20e93a142a7
Daniel Kaltenböck <daniel.kaltenboeck@linbit.com>
2019-05-21 14:14:32 +0200

    REST-client,doc: document resource.go
    Signed-off-by: Roland Kammerer <roland.kammerer@linbit.com>

--- client/resource.go
+++ client/resource.go
@@ -26,12 +26,14 @@ import (
 	"strconv"
 )
 
+// ResourceService is a struct which contains the pointer of the client
 type ResourceService struct {
 	client *Client
 }
 
 // copy & paste from generated code
 
+// Resource is a struct which holds the information of a resource
 type Resource struct {
 	Name     string `json:"name,omitempty"`
 	NodeName string `json:"node_name,omitempty"`
@@ -42,12 +44,14 @@ type Resource struct {
 	State       ResourceState     `json:"state,omitempty"`
 }
 
+// ResourceCreate is a struct where the properties of a resource are stored to create it
 type ResourceCreate struct {
 	Resource   Resource    `json:"resource,omitempty"`
 	LayerList  []LayerType `json:"layer_list,omitempty"`
 	DrbdNodeId int32       `json:"drbd_node_id,omitempty"`
 }
 
+// ResourceLayer is a struct to store layer-information abour a resource
 type ResourceLayer struct {
 	Children           []ResourceLayer `json:"children,omitempty"`
 	ResourceNameSuffix string          `json:"resource_name_suffix,omitempty"`
@@ -57,6 +61,7 @@ type ResourceLayer struct {
 	Storage            StorageResource `json:"storage,omitempty"`
 }
 
+// DrbdResource is a struct used to give linstor drbd properties for a resource
 type DrbdResource struct {
 	DrbdResourceDefinition DrbdResourceDefinitionLayer `json:"drbd_resource_definition,omitempty"`
 	NodeId                 int32                       `json:"node_id,omitempty"`
@@ -67,6 +72,7 @@ type DrbdResource struct {
 	DrbdVolumes            []DrbdVolume                `json:"drbd_volumes,omitempty"`
 }
 
+// DrbdVolume is a struct for linstor to get inormation about a drbd-volume
 type DrbdVolume struct {
 	DrbdVolumeDefinition DrbdVolumeDefinition `json:"drbd_volume_definition,omitempty"`
 	// drbd device path e.g. '/dev/drbd1000'
@@ -80,10 +86,12 @@ type DrbdVolume struct {
 	DiskState string `json:"disk_state,omitempty"`
 }
 
+// LuksResource is a struct to store storage-volumes for a luks-resource
 type LuksResource struct {
 	StorageVolumes []LuksVolume `json:"storage_volumes,omitempty"`
 }
 
+// LuksVolume is a struct used for information about a luks-volume
 type LuksVolume struct {
 	VolumeNumber int32 `json:"volume_number,omitempty"`
 	// block device path
@@ -97,10 +105,12 @@ type LuksVolume struct {
 	Opened    bool   `json:"opened,omitempty"`
 }
 
+// StorageResource is a struct which contains the storage-volumes for a storage-resource
 type StorageResource struct {
 	StorageVolumes []StorageVolume `json:"storage_volumes,omitempty"`
 }
 
+// StorageVolume is a struct to store standard poperties of a Volume
 type StorageVolume struct {
 	VolumeNumber int32 `json:"volume_number,omitempty"`
 	// block device path
@@ -111,10 +121,12 @@ type StorageVolume struct {
 	DiskState string `json:"disk_state,omitempty"`
 }
 
+// ResourceState is a struct for getting the status of a resource
 type ResourceState struct {
 	InUse bool `json:"in_use,omitempty"`
 }
 
+// Volume is a struct which holds the information about a linstor-volume
 type Volume struct {
 	VolumeNumber     int32        `json:"volume_number,omitempty"`
 	StoragePool      string       `json:"storage_pool,omitempty"`
@@ -129,21 +141,25 @@ type Volume struct {
 	LayerDataList []VolumeLayer     `json:"layer_data_list,omitempty"`
 }
 
+// VolumeLayer is a struct for storing the layer-properties of a linstor-volume
 type VolumeLayer struct {
 	Type LayerType                              `json:"type,omitempty"`
 	Data OneOfDrbdVolumeLuksVolumeStorageVolume `json:"data,omitempty"`
 }
 
+// VolumeState is a struct which contains the disk-state for volume
 type VolumeState struct {
 	DiskState string `json:"disk_state,omitempty"`
 }
 
+// AutoPlaceRequest is a struct to store the paramters for the linstor auto-place command
 type AutoPlaceRequest struct {
 	DisklessOnRemaining bool             `json:"diskless_on_remaining,omitempty"`
 	SelectFilter        AutoSelectFilter `json:"select_filter,omitempty"`
 	LayerList           []LayerType      `json:"layer_list,omitempty"`
 }
 
+// AutoSelectFilter is a struct used to have information about the auto-select function
 type AutoSelectFilter struct {
 	PlaceCount           int32    `json:"place_count,omitempty"`
 	StoragePool          string   `json:"storage_pool,omitempty"`
@@ -153,6 +169,7 @@ type AutoSelectFilter struct {
 	ReplicasOnDifferent  []string `json:"replicas_on_different,omitempty"`
 }
 
+// ResourceConnection is a struct which holds information about a connection between to nodes
 type ResourceConnection struct {
 	// source node of the connection
 	NodeA string `json:"node_a,omitempty"`
@@ -164,6 +181,7 @@ type ResourceConnection struct {
 	Port  int32             `json:"port,omitempty"`
 }
 
+// Snapshot is a struct for information about a snapshot
 type Snapshot struct {
 	Name         string   `json:"name,omitempty"`
 	ResourceName string   `json:"resource_name,omitempty"`
@@ -174,12 +192,14 @@ type Snapshot struct {
 	VolumeDefinitions []SnapshotVolumeDefinition `json:"volume_definitions,omitempty"`
 }
 
+// SnapshotVolumeDefinition is a struct to store the properties of a volume from a snapshot
 type SnapshotVolumeDefinition struct {
 	VolumeNumber int32 `json:"volume_number,omitempty"`
 	// Volume size in KiB
 	SizeKib uint64 `json:"size_kib,omitempty"`
 }
 
+// SnapshotRestore is a struct used to hold the information about where a Snapshot has to be restored
 type SnapshotRestore struct {
 	// Resource where to restore the snapshot
 	ToResource string `json:"to_resource"`
@@ -188,11 +208,14 @@ type SnapshotRestore struct {
 }
 
 // custom code
+
+// volumeLayerIn is a struct for volume-layers
 type volumeLayerIn struct {
 	Type LayerType       `json:"type,omitempty"`
 	Data json.RawMessage `json:"data,omitempty"`
 }
 
+// UnmarshalJSON fulfills the unmarshal interface for the VolumeLayer type
 func (v *VolumeLayer) UnmarshalJSON(b []byte) error {
 	var vIn volumeLayerIn
 	if err := json.Unmarshal(b, &vIn); err != nil {
@@ -226,41 +249,49 @@ func (v *VolumeLayer) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
+// OneOfDrbdVolumeLuksVolumeStorageVolume  is used to prevent that other types than drbd- luks- and storage-volume are used for a VolumeLayer
 type OneOfDrbdVolumeLuksVolumeStorageVolume interface {
 	isOneOfDrbdVolumeLuksVolumeStorageVolume()
 }
 
+// Functions which are used if type is a correct VolumeLayer
 func (d *DrbdVolume) isOneOfDrbdVolumeLuksVolumeStorageVolume()    {}
 func (d *LuksVolume) isOneOfDrbdVolumeLuksVolumeStorageVolume()    {}
 func (d *StorageVolume) isOneOfDrbdVolumeLuksVolumeStorageVolume() {}
 
+// GetAll returns all resources for a resource-definition
 func (n *ResourceService) GetAll(ctx context.Context, resName string, opts ...*ListOpts) ([]Resource, error) {
 	var reses []Resource
 	_, err := n.client.doGET(ctx, "/v1/resource-definitions/"+resName+"/resources", &reses, opts...)
 	return reses, err
 }
 
+// Get returns information about a resource on a specific node
 func (n *ResourceService) Get(ctx context.Context, resName, nodeName string, opts ...*ListOpts) (Resource, error) {
 	var res Resource
 	_, err := n.client.doGET(ctx, "/v1/resource-definitions/"+resName+"/resources/"+nodeName, &res, opts...)
 	return res, err
 }
 
+// Create is used to create a resource on a node
 func (n *ResourceService) Create(ctx context.Context, res ResourceCreate) error {
 	_, err := n.client.doPOST(ctx, "/v1/resource-definitions/"+res.Resource.Name+"/resources/"+res.Resource.NodeName, res)
 	return err
 }
 
+// Modify gives the ability to modify a resource on a node
 func (n *ResourceService) Modify(ctx context.Context, resName, nodeName string, props PropsModify) error {
 	_, err := n.client.doPUT(ctx, "/v1/resource-definitions/"+resName+"/resources/"+nodeName, props)
 	return err
 }
 
+// Delete deletes a resource on a specific node
 func (n *ResourceService) Delete(ctx context.Context, resName, nodeName string) error {
 	_, err := n.client.doDELETE(ctx, "/v1/resource-definitions/"+resName+"/resources/"+nodeName, nil)
 	return err
 }
 
+// GetVolumes lists als volumes of a resource
 func (n *ResourceService) GetVolumes(ctx context.Context, resName, nodeName string, opts ...*ListOpts) ([]Volume, error) {
 	var vols []Volume
 
@@ -268,6 +299,7 @@ func (n *ResourceService) GetVolumes(ctx context.Context, resName, nodeName stri
 	return vols, err
 }
 
+// GetVolume returns information about a specific volume defined by it resource,node and volume-number
 func (n *ResourceService) GetVolume(ctx context.Context, resName, nodeName string, volNr int, opts ...*ListOpts) (Volume, error) {
 	var vol Volume
 
@@ -275,6 +307,7 @@ func (n *ResourceService) GetVolume(ctx context.Context, resName, nodeName strin
 	return vol, err
 }
 
+// Diskless toggles a resource on a node to diskless - the parameter disklesspool can be set if its needed
 func (n *ResourceService) Diskless(ctx context.Context, resName, nodeName, disklessPoolName string) error {
 	u := "/v1/resource-definitions/" + resName + "/resources/" + nodeName + "/toggle-disk/diskless"
 	if disklessPoolName != "" {
@@ -284,6 +317,7 @@ func (n *ResourceService) Diskless(ctx context.Context, resName, nodeName, diskl
 	return err
 }
 
+// Diskful toggles a resource to diskful - the parameter storagepool can be set if its needed
 func (n *ResourceService) Diskful(ctx context.Context, resName, nodeName, storagePoolName string) error {
 	u := "/v1/resource-definitions/" + resName + "/resources/" + nodeName + "/toggle-disk/diskful"
 	if storagePoolName != "" {
@@ -293,6 +327,7 @@ func (n *ResourceService) Diskful(ctx context.Context, resName, nodeName, storag
 	return err
 }
 
+// Migrate mirgates a resource from one node to another node
 func (n *ResourceService) Migrate(ctx context.Context, resName, fromNodeName, toNodeName, storagePoolName string) error {
 	u := "/v1/resource-definitions/" + resName + "/resources/" + toNodeName + "/migrate-disk/" + toNodeName
 	if storagePoolName != "" {
@@ -302,11 +337,13 @@ func (n *ResourceService) Migrate(ctx context.Context, resName, fromNodeName, to
 	return err
 }
 
+// Autoplace places a resource on your nodes autmatically
 func (n *ResourceService) Autoplace(ctx context.Context, resName string, apr AutoPlaceRequest) error {
 	_, err := n.client.doPOST(ctx, "/v1/resource-definitions/"+resName+"/autoplace", apr)
 	return err
 }
 
+// GetConnections lists all resource connections if no node-names are given- if two node-names are given it shows the connection between them
 func (n *ResourceService) GetConnections(ctx context.Context, resName, nodeAName, nodeBName string, opts ...*ListOpts) ([]ResourceConnection, error) {
 	var resConns []ResourceConnection
 
@@ -319,12 +356,14 @@ func (n *ResourceService) GetConnections(ctx context.Context, resName, nodeAName
 	return resConns, err
 }
 
+// ModifyConnection allows to modify the connection between two nodes
 func (n *ResourceService) ModifyConnection(ctx context.Context, resName, nodeAName, nodeBName string, props PropsModify) error {
 	u := fmt.Sprintf("/v1/resource-definitions/%s/resource-connections/%s/%s", resName, nodeAName, nodeBName)
 	_, err := n.client.doPUT(ctx, u, props)
 	return err
 }
 
+// GetSnapshots lists all snapshots of a resource
 func (n *ResourceService) GetSnapshots(ctx context.Context, resName string, opts ...*ListOpts) ([]Snapshot, error) {
 	var snaps []Snapshot
 
@@ -332,6 +371,7 @@ func (n *ResourceService) GetSnapshots(ctx context.Context, resName string, opts
 	return snaps, err
 }
 
+// GetSnapshot returns information about a specific Snapshot by its name
 func (n *ResourceService) GetSnapshot(ctx context.Context, resName, snapName string, opts ...*ListOpts) (Snapshot, error) {
 	var snap Snapshot
 
@@ -339,46 +379,55 @@ func (n *ResourceService) GetSnapshot(ctx context.Context, resName, snapName str
 	return snap, err
 }
 
+// CreateSnapshot creates a snapshot of a resource
 func (n *ResourceService) CreateSnapshot(ctx context.Context, snapshot Snapshot) error {
 	_, err := n.client.doPOST(ctx, "/v1/resource-definitions/"+snapshot.ResourceName+"/snapshots", snapshot)
 	return err
 }
 
+// DeleteSnapshot deletes a snapshot by its name
 func (n *ResourceService) DeleteSnapshot(ctx context.Context, resName, snapName string) error {
 	_, err := n.client.doDELETE(ctx, "/v1/resource-definitions/"+resName+"/snapshots/"+snapName, nil)
 	return err
 }
 
+// RestoreSnapshot restores a snapshot on a resource
 func (n *ResourceService) RestoreSnapshot(ctx context.Context, origResName, snapName string, snapRestoreConf SnapshotRestore) error {
 	_, err := n.client.doPOST(ctx, "/v1/resource-definitions/"+origResName+"/snapshot-restore-resource/"+snapName, snapRestoreConf)
 	return err
 }
 
+// RestoreVolumeDefinitionSnapshot restores a volume-definition-snapshot on a resource
 func (n *ResourceService) RestoreVolumeDefinitionSnapshot(ctx context.Context, origResName, snapName string, snapRestoreConf SnapshotRestore) error {
-	_, err := n.client.doPOST(ctx, "/v1/resource-definitions/"+origResName+"/snapshot-restore-resource/"+snapName, snapRestoreConf)
+	_, err := n.client.doPOST(ctx, "/v1/resource-definitions/"+origResName+"/snapshot-restore-volume-definition/"+snapName, snapRestoreConf)
 	return err
 }
 
+// RollbackSnapshot rolls back a snapshot from a specific resource
 func (n *ResourceService) RollbackSnapshot(ctx context.Context, resName, snapName string) error {
 	_, err := n.client.doPOST(ctx, "/v1/resource-definitions/"+resName+"/snapshot-rollback/"+snapName, nil)
 	return err
 }
 
+//  ModifyDRBDProxy  is used to modify drbd-proxy properties
 func (n *ResourceService) ModifyDRBDProxy(ctx context.Context, resName string, props PropsModify) error {
 	_, err := n.client.doPUT(ctx, "/v1/resource-definitions/"+resName+"/drbd-proxy", props)
 	return err
 }
 
+// enableDisableDRBDProxy enables or disables drbd-proxy between two nodes
 func (n *ResourceService) enableDisableDRBDProxy(ctx context.Context, what, resName, nodeAName, nodeBName string) error {
 	u := fmt.Sprintf("/v1/resource-definitions/%s/drbd-proxy/%s/%s/%s", resName, what, nodeAName, nodeBName)
 	_, err := n.client.doPOST(ctx, u, nil)
 	return err
 }
 
+// EnableDRBDProxy is used to enable drbd-proxy with the rest-api call from the function enableDisableDRBDProxy
 func (n *ResourceService) EnableDRBDProxy(ctx context.Context, resName, nodeAName, nodeBName string) error {
 	return n.enableDisableDRBDProxy(ctx, "enable", resName, nodeAName, nodeBName)
 }
 
+// DisableDRBDProxy is used to disable drbd-proxy with the rest-api call from the function enableDisableDRBDProxy
 func (n *ResourceService) DisableDRBDProxy(ctx context.Context, resName, nodeAName, nodeBName string) error {
 	return n.enableDisableDRBDProxy(ctx, "disable", resName, nodeAName, nodeBName)
 }
