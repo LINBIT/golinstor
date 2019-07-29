b965b7011863fb2e05901df35784c78731c30e5e
Roland Kammerer <roland.kammerer@linbit.com>
2019-07-29 10:22:52 +0200

    REST-client: add https/ldap(s) examples
    I guess the https example is pretty obvious. The user always could
    specify its http.Client, so it is also its responsibility to set the
    client accordingly.
    
    Currently User/Password are sent via BasicAuth. As of now, this is only
    used for LDAP auth.

--- client/example_test.go
+++ client/example_test.go
@@ -2,16 +2,18 @@ package client_test
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 	"log"
+	"net/http"
 	"net/url"
 
 	"github.com/LINBIT/golinstor/client"
 	"github.com/sirupsen/logrus"
 )
 
-func Example() {
-	ctx := context.Background()
+func ExampleSimple() {
+	ctx := context.TODO()
 
 	u, err := url.Parse("http://controller:3370")
 	if err != nil {
@@ -23,6 +25,73 @@ func Example() {
 	}
 
 	c, err := client.NewClient(client.BaseURL(u), client.Log(logCfg))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rs, err := c.Resources.GetAll(ctx, "foo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, r := range rs {
+		fmt.Printf("Resource with name '%s' on node with name '%s'\n", r.Name, r.NodeName)
+	}
+}
+
+func ExampleHTTPS() {
+	ctx := context.TODO()
+
+	u, err := url.Parse("https://controller:3371")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Be careful if that is really what you want!
+	trSkipVerify := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+	httpClient := &http.Client{
+		Transport: trSkipVerify,
+	}
+
+	c, err := client.NewClient(client.BaseURL(u), client.HTTPClient(httpClient))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rs, err := c.Resources.GetAll(ctx, "foo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, r := range rs {
+		fmt.Printf("Resource with name '%s' on node with name '%s'\n", r.Name, r.NodeName)
+	}
+}
+
+func ExampleHTTPSLDAP() {
+	ctx := context.TODO()
+
+	u, err := url.Parse("https://controller:3371")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Be careful if that is really what you want!
+	trSkipVerify := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+	httpClient := &http.Client{
+		Transport: trSkipVerify,
+	}
+
+	c, err := client.NewClient(client.BaseURL(u), client.HTTPClient(httpClient),
+		client.BasicAuth(&client.BasicAuthCfg{Username: "Username", Password: "Password"}))
 	if err != nil {
 		log.Fatal(err)
 	}
