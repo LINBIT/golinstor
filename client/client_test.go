356c9e101b4fc42f23454c85645fb619fe29b8a3
Moritz "WanzenBug" Wanzenböck <moritz.wanzenboeck@linbit.com>
2020-06-18 17:48:05 +0200

    robust client configuration from environment
    handle client configuration from environment variables in a more
    predictable way. It is now possible to:
    * specify full URLS
    * specify only host + port
    * specify only scheme + host
    * specify only host
    * specify only CA without client authentication
    * specify only client authentication with default CA from host

---
+++ client/client_test.go
@@ -0,0 +1,112 @@
+package client
+
+import (
+	"os"
+	"testing"
+)
+
+const TestCaCert = `-----BEGIN CERTIFICATE-----
+MIIB1DCCAXqgAwIBAgIUZJyvXb6pJ9tHxKOlVkjTaqjpAAIwCgYIKoZIzj0EAwIw
+SDELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1TYW4gRnJhbmNp
+c2NvMRQwEgYDVQQDEwtleGFtcGxlLm5ldDAeFw0yMDA1MDgxMjUyMDBaFw0yNTA1
+MDcxMjUyMDBaMEgxCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMN
+U2FuIEZyYW5jaXNjbzEUMBIGA1UEAxMLZXhhbXBsZS5uZXQwWTATBgcqhkjOPQIB
+BggqhkjOPQMBBwNCAAT7uWd8BeFIcw64pRJheVh6tKrsqSLF4z9LAQKEaH5pg34+
+06T2Ed7hKUSca3R8zEuP9EZcHNpYXKoeuF1QjZt5o0IwQDAOBgNVHQ8BAf8EBAMC
+AQYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUyK850E3ZE9Jb3JPDq3BtttXd
+wE0wCgYIKoZIzj0EAwIDSAAwRQIge+3tsEmO/WbjxmQoA+kDoSpOQDnkDckqvEXD
+1H939HUCIQDHp0oAvI1sMM0ksAl7D0Bpxjtha2kpzbqsAf4yDy9rWw==
+-----END CERTIFICATE-----
+`
+
+func TestNewClient_ViaEnv(t *testing.T) {
+	testcases := []struct {
+		name        string
+		env         map[string]string
+		expectedUrl string
+		hasError    bool
+	}{
+		{
+			name:        "default",
+			expectedUrl: "http://localhost:3370",
+		},
+		{
+			name: "just-domain-name",
+			env:  map[string]string{"LS_CONTROLLERS": "just.domain"},
+			expectedUrl: "http://just.domain:3370",
+		},
+		{
+			name: "linstor-protocol",
+			env:  map[string]string{"LS_CONTROLLERS": "linstor://just.domain"},
+			expectedUrl: "http://just.domain:3370",
+		},
+		{
+			name: "just-domain-with-port",
+			env:  map[string]string{"LS_CONTROLLERS": "just.domain:4000"},
+			expectedUrl: "http://just.domain:4000",
+		},
+		{
+			name: "domain-with-protocol",
+			env:  map[string]string{"LS_CONTROLLERS": "http://just.domain"},
+			expectedUrl: "http://just.domain:3370",
+		},
+		{
+			name: "just-domain-with-https-protocol",
+			env:  map[string]string{"LS_CONTROLLERS": "https://just.domain"},
+			expectedUrl: "https://just.domain:3371",
+		},
+		{
+			name: "just-domain-with-client-secrets",
+			env:  map[string]string{"LS_CONTROLLERS": "just.domain", "LS_ROOT_CA": TestCaCert},
+			expectedUrl: "https://just.domain:3371",
+		},
+		{
+			name: "just-domain-with-client-secrets-and-port",
+			env:  map[string]string{"LS_CONTROLLERS": "just.domain:4000", "LS_ROOT_CA": TestCaCert},
+			expectedUrl: "https://just.domain:4000",
+		},
+		{
+			name: "parse-error-multi-scheme",
+			env:  map[string]string{"LS_CONTROLLERS": "https://http://just.domain:4000"},
+			hasError: true,
+		},
+		{
+			name: "parse-error-multi-port",
+			env:  map[string]string{"LS_CONTROLLERS": "https://just.domain:4000:5000"},
+			hasError: true,
+		},
+		{
+			name: "parse-error-inconsistent-env",
+			env:  map[string]string{"LS_CONTROLLERS": "https://just.domain:4000", "LS_USER_CERTIFICATE": "stuff"},
+			hasError: true,
+		},
+		{
+			name: "parse-error-inconsistent-env-other",
+			env:  map[string]string{"LS_CONTROLLERS": "https://just.domain:4000", "LS_USER_KEY": "stuff"},
+			hasError: true,
+		},
+	}
+
+	for _, item := range testcases {
+		test := item
+		t.Run(test.name, func(t *testing.T) {
+			os.Clearenv()
+			for k, v := range test.env {
+				_ = os.Setenv(k, v)
+			}
+
+			actual, err := NewClient()
+
+			if actual == nil {
+				if !test.hasError {
+					t.Errorf("expected no error, got error: %v", err)
+				}
+				return
+			}
+
+			if test.expectedUrl != actual.baseURL.String() {
+				t.Errorf("expected url: %v, got url: %v", test.expectedUrl, actual.baseURL.String())
+			}
+		})
+	}
+}
