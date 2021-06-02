LINBIT/golinstor

commit 110e5f8e9cfffdf69ff1b9181dadf3ac17c8015a

switch to generated consts
Use the consts generated by linstor-common for
* devicelayers
* snapshot-shipping-status
* node connection status

client/resource.go
@@ -22,6 +22,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"strconv"
+
+	"github.com/LINBIT/golinstor/devicelayerkind"
+	"github.com/LINBIT/golinstor/snapshotshipstatus"
 )
 
 // ResourceService is a struct which contains the pointer of the client
@@ -60,8 +63,8 @@ type ResourceDefinitionModify struct {
 	// drbd port for resources
 	DrbdPort int32 `json:"drbd_port,omitempty"`
 	// drbd peer slot number
-	DrbdPeerSlots int32       `json:"drbd_peer_slots,omitempty"`
-	LayerStack    []LayerType `json:"layer_stack,omitempty"`
+	DrbdPeerSlots int32                             `json:"drbd_peer_slots,omitempty"`
+	LayerStack    []devicelayerkind.DeviceLayerKind `json:"layer_stack,omitempty"`
 	// change resource group to the given group name
 	ResourceGroup string `json:"resource_group,omitempty"`
 	GenericPropsModify
@@ -69,23 +72,23 @@ type ResourceDefinitionModify struct {
 
 // ResourceCreate is a struct where the properties of a resource are stored to create it
 type ResourceCreate struct {
-	Resource   Resource    `json:"resource,omitempty"`
-	LayerList  []LayerType `json:"layer_list,omitempty"`
-	DrbdNodeId int32       `json:"drbd_node_id,omitempty"`
+	Resource   Resource                          `json:"resource,omitempty"`
+	LayerList  []devicelayerkind.DeviceLayerKind `json:"layer_list,omitempty"`
+	DrbdNodeId int32                             `json:"drbd_node_id,omitempty"`
 }
 
 // ResourceLayer is a struct to store layer-information abour a resource
 type ResourceLayer struct {
-	Children           []ResourceLayer    `json:"children,omitempty"`
-	ResourceNameSuffix string             `json:"resource_name_suffix,omitempty"`
-	Type               LayerType          `json:"type,omitempty"`
-	Drbd               DrbdResource       `json:"drbd,omitempty"`
-	Luks               LuksResource       `json:"luks,omitempty"`
-	Storage            StorageResource    `json:"storage,omitempty"`
-	Nvme               NvmeResource       `json:"nvme,omitempty"`
-	Openflex           OpenflexResource   `json:"openflex,omitempty"`
-	Writecache         WritecacheResource `json:"writecache,omitempty"`
-	Cache              CacheResource      `json:"cache,omitempty"`
+	Children           []ResourceLayer                 `json:"children,omitempty"`
+	ResourceNameSuffix string                          `json:"resource_name_suffix,omitempty"`
+	Type               devicelayerkind.DeviceLayerKind `json:"type,omitempty"`
+	Drbd               DrbdResource                    `json:"drbd,omitempty"`
+	Luks               LuksResource                    `json:"luks,omitempty"`
+	Storage            StorageResource                 `json:"storage,omitempty"`
+	Nvme               NvmeResource                    `json:"nvme,omitempty"`
+	Openflex           OpenflexResource                `json:"openflex,omitempty"`
+	Writecache         WritecacheResource              `json:"writecache,omitempty"`
+	Cache              CacheResource                   `json:"cache,omitempty"`
 }
 
 type WritecacheResource struct {
@@ -217,7 +220,7 @@ type Volume struct {
 
 // VolumeLayer is a struct for storing the layer-properties of a linstor-volume
 type VolumeLayer struct {
-	Type LayerType                                                                   `json:"type,omitempty"`
+	Type devicelayerkind.DeviceLayerKind                                             `json:"type,omitempty"`
 	Data OneOfDrbdVolumeLuksVolumeStorageVolumeNvmeVolumeWritecacheVolumeCacheVolume `json:"data,omitempty"`
 }
 
@@ -228,9 +231,9 @@ type VolumeState struct {
 
 // AutoPlaceRequest is a struct to store the paramters for the linstor auto-place command
 type AutoPlaceRequest struct {
-	DisklessOnRemaining bool             `json:"diskless_on_remaining,omitempty"`
-	SelectFilter        AutoSelectFilter `json:"select_filter,omitempty"`
-	LayerList           []LayerType      `json:"layer_list,omitempty"`
+	DisklessOnRemaining bool                              `json:"diskless_on_remaining,omitempty"`
+	SelectFilter        AutoSelectFilter                  `json:"select_filter,omitempty"`
+	LayerList           []devicelayerkind.DeviceLayerKind `json:"layer_list,omitempty"`
 }
 
 // AutoSelectFilter is a struct used to have information about the auto-select function
@@ -304,10 +307,10 @@ type SnapshotShipping struct {
 
 // SnapshotShippingStatus struct for SnapshotShippingStatus
 type SnapshotShippingStatus struct {
-	Snapshot     Snapshot `json:"snapshot,omitempty"`
-	FromNodeName string   `json:"from_node_name,omitempty"`
-	ToNodeName   string   `json:"to_node_name,omitempty"`
-	Status       string   `json:"status,omitempty"`
+	Snapshot     Snapshot                              `json:"snapshot,omitempty"`
+	FromNodeName string                                `json:"from_node_name,omitempty"`
+	ToNodeName   string                                `json:"to_node_name,omitempty"`
+	Status       snapshotshipstatus.SnapshotShipStatus `json:"status,omitempty"`
 }
 
 // SnapshotVolumeDefinition is a struct to store the properties of a volume from a snapshot
@@ -349,7 +352,7 @@ type MaxVolumeSizes struct {
 }
 
 type ResourceMakeAvailable struct {
-	LayerList []LayerType `json:"layer_list,omitempty"`
+	LayerList []devicelayerkind.DeviceLayerKind `json:"layer_list,omitempty"`
 	// if true resource will be created as diskful even if diskless would be possible
 	Diskful bool `json:"diskful,omitempty"`
 }
@@ -442,8 +445,8 @@ type ResourceProvider interface {
 
 // volumeLayerIn is a struct for volume-layers
 type volumeLayerIn struct {
-	Type LayerType       `json:"type,omitempty"`
-	Data json.RawMessage `json:"data,omitempty"`
+	Type devicelayerkind.DeviceLayerKind `json:"type,omitempty"`
+	Data json.RawMessage                 `json:"data,omitempty"`
 }
 
 // UnmarshalJSON fulfills the unmarshal interface for the VolumeLayer type
@@ -455,7 +458,7 @@ func (v *VolumeLayer) UnmarshalJSON(b []byte) error {
 
 	v.Type = vIn.Type
 	switch v.Type {
-	case DRBD:
+	case devicelayerkind.Drbd:
 		dst := new(DrbdVolume)
 		if vIn.Data != nil {
 			if err := json.Unmarshal(vIn.Data, &dst); err != nil {
@@ -463,7 +466,7 @@ func (v *VolumeLayer) UnmarshalJSON(b []byte) error {
 			}
 		}
 		v.Data = dst
-	case LUKS:
+	case devicelayerkind.Luks:
 		dst := new(LuksVolume)
 		if vIn.Data != nil {
 			if err := json.Unmarshal(vIn.Data, &dst); err != nil {
@@ -471,7 +474,7 @@ func (v *VolumeLayer) UnmarshalJSON(b []byte) error {
 			}
 		}
 		v.Data = dst
-	case STORAGE:
+	case devicelayerkind.Storage:
 		dst := new(StorageVolume)
 		if vIn.Data != nil {
 			if err := json.Unmarshal(vIn.Data, &dst); err != nil {
@@ -479,7 +482,7 @@ func (v *VolumeLayer) UnmarshalJSON(b []byte) error {
 			}
 		}
 		v.Data = dst
-	case NVME:
+	case devicelayerkind.Nvme:
 		dst := new(NvmeVolume)
 		if vIn.Data != nil {
 			if err := json.Unmarshal(vIn.Data, &dst); err != nil {
@@ -487,7 +490,7 @@ func (v *VolumeLayer) UnmarshalJSON(b []byte) error {
 			}
 		}
 		v.Data = dst
-	case WRITECACHE:
+	case devicelayerkind.Writecache:
 		dst := new(WritecacheVolume)
 		if vIn.Data != nil {
 			if err := json.Unmarshal(vIn.Data, &dst); err != nil {
@@ -495,6 +498,9 @@ func (v *VolumeLayer) UnmarshalJSON(b []byte) error {
 			}
 		}
 		v.Data = dst
+	case devicelayerkind.Cache:
+	case devicelayerkind.Openflex:
+	case devicelayerkind.Exos:
 	default:
 		return fmt.Errorf("'%+v' is not a valid type to Unmarshal", v.Type)
 	}

devicelayerkind/devicelayerkind.go
@@ -1,0 +1,14 @@
+package devicelayerkind
+
+type DeviceLayerKind string
+
+const (
+	Drbd       DeviceLayerKind = "drbd"
+	Luks       DeviceLayerKind = "luks"
+	Storage    DeviceLayerKind = "storage"
+	Nvme       DeviceLayerKind = "nvme"
+	Openflex   DeviceLayerKind = "openflex"
+	Exos       DeviceLayerKind = "exos"
+	Writecache DeviceLayerKind = "writecache"
+	Cache      DeviceLayerKind = "cache"
+)

client/node.go
@@ -16,7 +16,11 @@
 
 package client
 
-import "context"
+import (
+	"context"
+
+	"github.com/LINBIT/golinstor/devicelayerkind"
+)
 
 // copy & paste from generated code
 
@@ -31,11 +35,11 @@ type Node struct {
 	// Enum describing the current connection status.
 	ConnectionStatus string `json:"connection_status,omitempty"`
 	// unique object id
-	Uuid                 string                    `json:"uuid,omitempty"`
-	StorageProviders     []ProviderKind            `json:"storage_providers,omitempty"`
-	ResourceLayers       []LayerType               `json:"resource_layers,omitempty"`
-	UnsupportedProviders map[ProviderKind][]string `json:"unsupported_providers,omitempty"`
-	UnsupportedLayers    map[LayerType][]string    `json:"unsupported_layers,omitempty"`
+	Uuid                 string                                       `json:"uuid,omitempty"`
+	StorageProviders     []ProviderKind                               `json:"storage_providers,omitempty"`
+	ResourceLayers       []devicelayerkind.DeviceLayerKind            `json:"resource_layers,omitempty"`
+	UnsupportedProviders map[ProviderKind][]string                    `json:"unsupported_providers,omitempty"`
+	UnsupportedLayers    map[devicelayerkind.DeviceLayerKind][]string `json:"unsupported_layers,omitempty"`
 }
 
 type NodeModify struct {

client/resourcedefinition.go
@@ -22,6 +22,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"strconv"
+
+	"github.com/LINBIT/golinstor/devicelayerkind"
 )
 
 // ResourceDefinitionService is a struct for the client pointer
@@ -56,7 +58,7 @@ type ResourceDefinitionCreate struct {
 
 // ResourceDefinitionLayer is a struct for the storing the layertype of a resource-defintion
 type ResourceDefinitionLayer struct {
-	Type LayerType                                                       `json:"type,omitempty"`
+	Type devicelayerkind.DeviceLayerKind                                 `json:"type,omitempty"`
 	Data OneOfDrbdResourceDefinitionLayerOpenflexResourceDefinitionLayer `json:"data,omitempty"`
 }
 
@@ -72,20 +74,6 @@ type DrbdResourceDefinitionLayer struct {
 	Secret string `json:"secret,omitempty"`
 	Down   bool   `json:"down,omitempty"`
 }
-
-// LayerType initialized as string
-type LayerType string
-
-// List of LayerType
-const (
-	DRBD       LayerType = "DRBD"
-	LUKS       LayerType = "LUKS"
-	STORAGE    LayerType = "STORAGE"
-	NVME       LayerType = "NVME"
-	WRITECACHE LayerType = "WRITECACHE"
-	CACHE      LayerType = "CACHE"
-	OPENFLEX   LayerType = "OPENFLEX"
-)
 
 // VolumeDefinitionCreate is a struct used for creating volume-definitions
 type VolumeDefinitionCreate struct {
@@ -115,8 +103,8 @@ type VolumeDefinitionModify struct {
 
 // VolumeDefinitionLayer is a struct for the layer-type of a volume-definition
 type VolumeDefinitionLayer struct {
-	Type LayerType                 `json:"type"`
-	Data OneOfDrbdVolumeDefinition `json:"data,omitempty"`
+	Type devicelayerkind.DeviceLayerKind `json:"type"`
+	Data OneOfDrbdVolumeDefinition       `json:"data,omitempty"`
 }
 
 // DrbdVolumeDefinition is a struct containing volume-definition on drbd level
@@ -162,8 +150,8 @@ type ResourceDefinitionProvider interface {
 
 // resourceDefinitionLayerIn is a struct for resource-definitions
 type resourceDefinitionLayerIn struct {
-	Type LayerType       `json:"type,omitempty"`
-	Data json.RawMessage `json:"data,omitempty"`
+	Type devicelayerkind.DeviceLayerKind `json:"type,omitempty"`
+	Data json.RawMessage                 `json:"data,omitempty"`
 }
 
 // UnmarshalJSON is needed for the unmarshal interface for ResourceDefinitionLayer types
@@ -175,7 +163,7 @@ func (rd *ResourceDefinitionLayer) UnmarshalJSON(b []byte) error {
 
 	rd.Type = rdIn.Type
 	switch rd.Type {
-	case DRBD:
+	case devicelayerkind.Drbd:
 		dst := new(DrbdResourceDefinitionLayer)
 		if rdIn.Data != nil {
 			if err := json.Unmarshal(rdIn.Data, &dst); err != nil {
@@ -183,7 +171,7 @@ func (rd *ResourceDefinitionLayer) UnmarshalJSON(b []byte) error {
 			}
 		}
 		rd.Data = dst
-	case OPENFLEX:
+	case devicelayerkind.Openflex:
 		dst := new(OpenflexResourceDefinitionLayer)
 		if rdIn.Data != nil {
 			if err := json.Unmarshal(rdIn.Data, &dst); err != nil {
@@ -191,7 +179,7 @@ func (rd *ResourceDefinitionLayer) UnmarshalJSON(b []byte) error {
 			}
 		}
 		rd.Data = dst
-	case LUKS, STORAGE, NVME, WRITECACHE, CACHE: // valid types, but do not set data
+	case devicelayerkind.Luks, devicelayerkind.Storage, devicelayerkind.Nvme, devicelayerkind.Writecache, devicelayerkind.Cache, devicelayerkind.Exos: // valid types, but do not set data
 	default:
 		return fmt.Errorf("'%+v' is not a valid type to Unmarshal", rd.Type)
 	}
@@ -210,8 +198,8 @@ func (d *DrbdResourceDefinitionLayer) isOneOfDrbdResourceDefinitionLayerOpenflex
 
 //volumeDefinitionLayerIn is a struct for volume-defintion-layers
 type volumeDefinitionLayerIn struct {
-	Type LayerType       `json:"type,omitempty"`
-	Data json.RawMessage `json:"data,omitempty"`
+	Type devicelayerkind.DeviceLayerKind `json:"type,omitempty"`
+	Data json.RawMessage                 `json:"data,omitempty"`
 }
 
 // UnmarshalJSON is needed for the unmarshal interface for VolumeDefinitionLayer types
@@ -223,7 +211,7 @@ func (vd *VolumeDefinitionLayer) UnmarshalJSON(b []byte) error {
 
 	vd.Type = vdIn.Type
 	switch vd.Type {
-	case DRBD:
+	case devicelayerkind.Drbd:
 		dst := new(DrbdVolumeDefinition)
 		if vdIn.Data != nil {
 			if err := json.Unmarshal(vdIn.Data, &dst); err != nil {
@@ -231,7 +219,7 @@ func (vd *VolumeDefinitionLayer) UnmarshalJSON(b []byte) error {
 			}
 		}
 		vd.Data = dst
-	case LUKS, STORAGE, NVME, WRITECACHE: // valid types, but do not set data
+	case devicelayerkind.Luks, devicelayerkind.Storage, devicelayerkind.Nvme, devicelayerkind.Writecache, devicelayerkind.Openflex, devicelayerkind.Cache, devicelayerkind.Exos: // valid types, but do not set data
 	default:
 		return fmt.Errorf("'%+v' is not a valid type to Unmarshal", vd.Type)
 	}

connectionstatus/connectionstatus.go
@@ -1,0 +1,17 @@
+package connectionstatus
+
+type ConnectionStatus int
+
+const (
+	Offline             ConnectionStatus = 0
+	Connected           ConnectionStatus = 1
+	Online              ConnectionStatus = 2
+	VersionMismatch     ConnectionStatus = 3
+	FullSyncFailed      ConnectionStatus = 4
+	AuthenticationError ConnectionStatus = 5
+	Unknown             ConnectionStatus = 6
+	HostnameMismatch    ConnectionStatus = 7
+	OtherController     ConnectionStatus = 8
+	Authenticated       ConnectionStatus = 9
+	NoStltConn          ConnectionStatus = 10
+)

commit a415dddf866aa18da510a14ff08f636050a8eeef

fix parsing of volume responses
Parsing of volumes relies on the existing (generated) enum names to align
with the response of the Linstor API. The generated enum values did not
completely align with the Linstor API, so normal responses ended up not
parsing.

The fix is to change the enum values in the linstor-common project, as they
should always align with the Linstor API. A rerun of go generate fixes the
issue. To ensure parsing of those resources does not break again, some new
tests were added.

This bug was introduced in 110e5f8e9cf while switching to the new style
enum generation. Before that commit, the previous values were the once
returned by Linstor.

apiconsts.go
@@ -504,6 +504,16 @@ const KeyCacheBlockSize = "Blocksize"
 const KeyCachePolicy = "Policy"
 const KeyUpdateCacheInterval = "UpdateCacheInterval"
 
+// ## BCache property keys ###
+const KeyBcachePoolName = "PoolName"
+const KeyBcacheSize = "Size"
+const KeyBcacheBlocksize = "Blocksize"
+const KeyBcacheBucketsize = "Bucketsize"
+const KeyBcacheDataOffset = "DataOffset"
+const KeyBcacheWriteback = "Writeback"
+const KeyBcacheDiscard = "Discard"
+const KeyBcacheCacheReplacementPolicy = "CacheReplacementPolicy"
+
 // ## Autoplace property keys ###
 const KeyAutoplaceStratWeightMaxFreespace = "MaxFreeSpace"
 const KeyAutoplaceStratWeightMinReservedSpace = "MinReservedSpace"
@@ -558,6 +568,7 @@ const NamespcWritecacheOptions = "Writecache/Options"
 const NamespcCache = "Cache"
 const NamespcCacheFeatures = "Cache/Features"
 const NamespcCachePolicyArgs = "Cache/Policy"
+const NamespcBcache = "BCache"
 const NamespcAutoplacer = "Autoplacer"
 const NamespcAutoplacerWeights = "Autoplacer/Weights"
 const NamespcSnapshotShipping = "SnapshotShipping"
@@ -721,14 +732,14 @@ const FlagRscInactive = "INACTIVE"
 
 // ## Device layer kinds ###
 // enum generated in package -> "golinstor/devicelayerkind"
-// devicelayerkind.Drbd = "drbd"
-// devicelayerkind.Luks = "luks"
-// devicelayerkind.Storage = "storage"
-// devicelayerkind.Nvme = "nvme"
-// devicelayerkind.Openflex = "openflex"
-// devicelayerkind.Exos = "exos"
-// devicelayerkind.Writecache = "writecache"
-// devicelayerkind.Cache = "cache"
+// devicelayerkind.Drbd = "DRBD"
+// devicelayerkind.Luks = "LUKS"
+// devicelayerkind.Storage = "STORAGE"
+// devicelayerkind.Nvme = "NVME"
+// devicelayerkind.Openflex = "OPENFLEX"
+// devicelayerkind.Exos = "EXOS"
+// devicelayerkind.Writecache = "WRITECACHE"
+// devicelayerkind.Cache = "CACHE"
 
 // ## Satellite connection statuses ###
 // enum generated in package -> "golinstor/connectionstatus"

client/resource.go
@@ -609,6 +609,7 @@ func (n *ResourceService) Diskless(ctx context.Context, resName, nodeName, diskl
 	if disklessPoolName != "" {
 		u += "/" + disklessPoolName
 	}
+
 	_, err := n.client.doPUT(ctx, u, nil)
 	return err
 }

devicelayerkind/devicelayerkind.go
@@ -3,12 +3,12 @@ package devicelayerkind
 type DeviceLayerKind string
 
 const (
-	Drbd       DeviceLayerKind = "drbd"
-	Luks       DeviceLayerKind = "luks"
-	Storage    DeviceLayerKind = "storage"
-	Nvme       DeviceLayerKind = "nvme"
-	Openflex   DeviceLayerKind = "openflex"
-	Exos       DeviceLayerKind = "exos"
-	Writecache DeviceLayerKind = "writecache"
-	Cache      DeviceLayerKind = "cache"
+	Drbd       DeviceLayerKind = "DRBD"
+	Luks       DeviceLayerKind = "LUKS"
+	Storage    DeviceLayerKind = "STORAGE"
+	Nvme       DeviceLayerKind = "NVME"
+	Openflex   DeviceLayerKind = "OPENFLEX"
+	Exos       DeviceLayerKind = "EXOS"
+	Writecache DeviceLayerKind = "WRITECACHE"
+	Cache      DeviceLayerKind = "CACHE"
 )

client/resource_test.go
@@ -1,0 +1,253 @@
+package client_test
+
+import (
+	"encoding/json"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/LINBIT/golinstor/client"
+	"github.com/LINBIT/golinstor/devicelayerkind"
+)
+
+func TestParse(t *testing.T) {
+	testcases := []struct {
+		response string
+		actual   interface{}
+		expected interface{}
+	}{
+		{
+			response: `{"name":"pvc-b5be6893-9892-4278-b2da-51a060fc4624","node_name":"demo1.linstor-days.at.linbit.com","props":{"StorPoolName":"thinpool"},"layer_object":{"children":[{"type":"STORAGE","storage":{"storage_volumes":[{"volume_number":0,"device_path":"/dev/linstor_thinpool/pvc-b5be6893-9892-4278-b2da-51a060fc4624_00000","allocated_size_kib":516096,"usable_size_kib":516096,"disk_state":"[]"}]}}],"type":"DRBD","drbd":{"drbd_resource_definition":{"peer_slots":7,"al_stripes":1,"al_stripe_size_kib":32,"port":7000,"transport_type":"IP","secret":"bNvYcSbPFPpbHZ9Gtq00","down":false},"node_id":0,"peer_slots":7,"al_stripes":1,"al_size":32,"drbd_volumes":[{"drbd_volume_definition":{"volume_number":0,"minor_number":1000},"device_path":"/dev/drbd1000","backing_device":"/dev/linstor_thinpool/pvc-b5be6893-9892-4278-b2da-51a060fc4624_00000","allocated_size_kib":512148,"usable_size_kib":512000}],"connections":{"demo2.linstor-days.at.linbit.com":{"connected":true,"message":"Connected"},"demo3.linstor-days.at.linbit.com":{"connected":false,"message":"Connecting"}},"promotion_score":10101,"may_promote":true}},"state":{"in_use":false},"uuid":"78f0d7fe-2b4d-4d5b-afb4-e1b1450c70cb","create_timestamp":1622636098831}`,
+			actual: &client.Resource{},
+			expected: &client.Resource{
+				Name:     "pvc-b5be6893-9892-4278-b2da-51a060fc4624",
+				NodeName: "demo1.linstor-days.at.linbit.com",
+				Props: map[string]string{
+					"StorPoolName": "thinpool",
+				},
+				LayerObject: client.ResourceLayer{
+					Children: []client.ResourceLayer{
+						{
+							Type: devicelayerkind.Storage,
+							Storage: client.StorageResource{
+								StorageVolumes: []client.StorageVolume{
+									{
+										VolumeNumber:     0,
+										DevicePath:       "/dev/linstor_thinpool/pvc-b5be6893-9892-4278-b2da-51a060fc4624_00000",
+										AllocatedSizeKib: 516096,
+										UsableSizeKib:    516096,
+										DiskState:        "[]",
+									},
+								},
+							},
+						},
+					},
+					Type: devicelayerkind.Drbd,
+					Drbd: client.DrbdResource{
+						DrbdResourceDefinition: client.DrbdResourceDefinitionLayer{
+							PeerSlots:     7,
+							AlStripes:     1,
+							Port:          7000,
+							TransportType: "IP",
+							Secret:        "bNvYcSbPFPpbHZ9Gtq00",
+						},
+						DrbdVolumes: []client.DrbdVolume{
+							{
+								DrbdVolumeDefinition: client.DrbdVolumeDefinition{
+									ResourceNameSuffix: "",
+									VolumeNumber:       0,
+									MinorNumber:        1000,
+								},
+								DevicePath:       "/dev/drbd1000",
+								BackingDevice:    "/dev/linstor_thinpool/pvc-b5be6893-9892-4278-b2da-51a060fc4624_00000",
+								MetaDisk:         "",
+								AllocatedSizeKib: 512148,
+								UsableSizeKib:    512000,
+								DiskState:        "",
+								ExtMetaStorPool:  "",
+							},
+						},
+						Connections: map[string]client.DrbdConnection{
+							"demo2.linstor-days.at.linbit.com": {
+								Connected: true,
+								Message:   "Connected",
+							},
+							"demo3.linstor-days.at.linbit.com": {
+								Connected: false,
+								Message:   "Connecting",
+							},
+						},
+						PeerSlots:      7,
+						AlStripes:      1,
+						AlSize:         32,
+						PromotionScore: 10101,
+						MayPromote:     true,
+					},
+				},
+				Uuid:            "78f0d7fe-2b4d-4d5b-afb4-e1b1450c70cb",
+				CreateTimestamp: 1622636098831,
+			},
+		},
+		{
+			response: `{"name":"pvc-b5be6893-9892-4278-b2da-51a060fc4624","node_name":"demo1.linstor-days.at.linbit.com","props":{"StorPoolName":"thinpool"},"layer_object":{"children":[{"type":"STORAGE","storage":{"storage_volumes":[{"volume_number":0,"device_path":"/dev/linstor_thinpool/pvc-b5be6893-9892-4278-b2da-51a060fc4624_00000","allocated_size_kib":516096,"usable_size_kib":516096,"disk_state":"[]"}]}}],"type":"DRBD","drbd":{"drbd_resource_definition":{"peer_slots":7,"al_stripes":1,"al_stripe_size_kib":32,"port":7000,"transport_type":"IP","secret":"bNvYcSbPFPpbHZ9Gtq00","down":false},"node_id":0,"peer_slots":7,"al_stripes":1,"al_size":32,"drbd_volumes":[{"drbd_volume_definition":{"volume_number":0,"minor_number":1000},"device_path":"/dev/drbd1000","backing_device":"/dev/linstor_thinpool/pvc-b5be6893-9892-4278-b2da-51a060fc4624_00000","allocated_size_kib":512148,"usable_size_kib":512000}],"connections":{"demo2.linstor-days.at.linbit.com":{"connected":true,"message":"Connected"},"demo3.linstor-days.at.linbit.com":{"connected":false,"message":"Connecting"}},"promotion_score":10101,"may_promote":true}},"state":{"in_use":false},"uuid":"78f0d7fe-2b4d-4d5b-afb4-e1b1450c70cb","create_timestamp":1622636098831,"volumes":[{"volume_number":0,"storage_pool_name":"thinpool","provider_kind":"LVM_THIN","device_path":"/dev/drbd1000","allocated_size_kib":206,"state":{"disk_state":"UpToDate"},"layer_data_list":[{"type":"DRBD","data":{"drbd_volume_definition":{"volume_number":0,"minor_number":1000},"device_path":"/dev/drbd1000","backing_device":"/dev/linstor_thinpool/pvc-b5be6893-9892-4278-b2da-51a060fc4624_00000","allocated_size_kib":512148,"usable_size_kib":512000}},{"type":"STORAGE","data":{"volume_number":0,"device_path":"/dev/linstor_thinpool/pvc-b5be6893-9892-4278-b2da-51a060fc4624_00000","allocated_size_kib":516096,"usable_size_kib":516096,"disk_state":"[]"}}],"uuid":"03b8ffd6-dbef-4745-87a0-46b4f8459e1e"}]}`,
+			actual: &client.ResourceWithVolumes{},
+			expected: &client.ResourceWithVolumes{
+				Resource: client.Resource{
+					Name:     "pvc-b5be6893-9892-4278-b2da-51a060fc4624",
+					NodeName: "demo1.linstor-days.at.linbit.com",
+					Props: map[string]string{
+						"StorPoolName": "thinpool",
+					},
+					LayerObject: client.ResourceLayer{
+						Children: []client.ResourceLayer{
+							{
+								Type: devicelayerkind.Storage,
+								Storage: client.StorageResource{
+									StorageVolumes: []client.StorageVolume{
+										{
+											VolumeNumber:     0,
+											DevicePath:       "/dev/linstor_thinpool/pvc-b5be6893-9892-4278-b2da-51a060fc4624_00000",
+											AllocatedSizeKib: 516096,
+											UsableSizeKib:    516096,
+											DiskState:        "[]",
+										},
+									},
+								},
+							},
+						},
+						Type: devicelayerkind.Drbd,
+						Drbd: client.DrbdResource{
+							DrbdResourceDefinition: client.DrbdResourceDefinitionLayer{
+								PeerSlots:     7,
+								AlStripes:     1,
+								Port:          7000,
+								TransportType: "IP",
+								Secret:        "bNvYcSbPFPpbHZ9Gtq00",
+							},
+							DrbdVolumes: []client.DrbdVolume{
+								{
+									DrbdVolumeDefinition: client.DrbdVolumeDefinition{
+										ResourceNameSuffix: "",
+										VolumeNumber:       0,
+										MinorNumber:        1000,
+									},
+									DevicePath:       "/dev/drbd1000",
+									BackingDevice:    "/dev/linstor_thinpool/pvc-b5be6893-9892-4278-b2da-51a060fc4624_00000",
+									MetaDisk:         "",
+									AllocatedSizeKib: 512148,
+									UsableSizeKib:    512000,
+									DiskState:        "",
+									ExtMetaStorPool:  "",
+								},
+							},
+							Connections: map[string]client.DrbdConnection{
+								"demo2.linstor-days.at.linbit.com": {
+									Connected: true,
+									Message:   "Connected",
+								},
+								"demo3.linstor-days.at.linbit.com": {
+									Connected: false,
+									Message:   "Connecting",
+								},
+							},
+							PeerSlots:      7,
+							AlStripes:      1,
+							AlSize:         32,
+							PromotionScore: 10101,
+							MayPromote:     true,
+						},
+					},
+					Uuid: "78f0d7fe-2b4d-4d5b-afb4-e1b1450c70cb",
+				},
+				CreateTimestamp: 1622636098831,
+				Volumes: []client.Volume{
+					{
+						StoragePoolName:  "thinpool",
+						ProviderKind:     client.LVM_THIN,
+						DevicePath:       "/dev/drbd1000",
+						AllocatedSizeKib: 206,
+						State: client.VolumeState{
+							DiskState: "UpToDate",
+						},
+						LayerDataList: []client.VolumeLayer{
+							{
+								Type: devicelayerkind.Drbd,
+								Data: &client.DrbdVolume{
+									DrbdVolumeDefinition: client.DrbdVolumeDefinition{
+										MinorNumber: 1000,
+									},
+									DevicePath:       "/dev/drbd1000",
+									BackingDevice:    "/dev/linstor_thinpool/pvc-b5be6893-9892-4278-b2da-51a060fc4624_00000",
+									AllocatedSizeKib: 512148,
+									UsableSizeKib:    512000,
+								},
+							},
+							{
+								Type: devicelayerkind.Storage,
+								Data: &client.StorageVolume{
+									DevicePath:       "/dev/linstor_thinpool/pvc-b5be6893-9892-4278-b2da-51a060fc4624_00000",
+									AllocatedSizeKib: 516096,
+									UsableSizeKib:    516096,
+									DiskState:        "[]",
+								},
+							},
+						},
+						Uuid: "03b8ffd6-dbef-4745-87a0-46b4f8459e1e",
+					},
+				},
+			},
+		},
+		{
+			response: `{"volume_number":0,"storage_pool_name":"thinpool","provider_kind":"LVM_THIN","device_path":"/dev/drbd1000","allocated_size_kib":206,"state":{"disk_state":"UpToDate"},"layer_data_list":[{"type":"DRBD","data":{"drbd_volume_definition":{"volume_number":0,"minor_number":1000},"device_path":"/dev/drbd1000","backing_device":"/dev/linstor_thinpool/pvc-b5be6893-9892-4278-b2da-51a060fc4624_00000","allocated_size_kib":512148,"usable_size_kib":512000}},{"type":"STORAGE","data":{"volume_number":0,"device_path":"/dev/linstor_thinpool/pvc-b5be6893-9892-4278-b2da-51a060fc4624_00000","allocated_size_kib":516096,"usable_size_kib":516096,"disk_state":"[]"}}],"uuid":"03b8ffd6-dbef-4745-87a0-46b4f8459e1e"}`,
+			actual: &client.Volume{},
+			expected: &client.Volume{
+				StoragePoolName:  "thinpool",
+				ProviderKind:     client.LVM_THIN,
+				DevicePath:       "/dev/drbd1000",
+				AllocatedSizeKib: 206,
+				State: client.VolumeState{
+					DiskState: "UpToDate",
+				},
+				LayerDataList: []client.VolumeLayer{
+					{
+						Type: devicelayerkind.Drbd,
+						Data: &client.DrbdVolume{
+							DrbdVolumeDefinition: client.DrbdVolumeDefinition{
+								MinorNumber: 1000,
+							},
+							DevicePath:       "/dev/drbd1000",
+							BackingDevice:    "/dev/linstor_thinpool/pvc-b5be6893-9892-4278-b2da-51a060fc4624_00000",
+							AllocatedSizeKib: 512148,
+							UsableSizeKib:    512000,
+						},
+					},
+					{
+						Type: devicelayerkind.Storage,
+						Data: &client.StorageVolume{
+							DevicePath:       "/dev/linstor_thinpool/pvc-b5be6893-9892-4278-b2da-51a060fc4624_00000",
+							AllocatedSizeKib: 516096,
+							UsableSizeKib:    516096,
+							DiskState:        "[]",
+						},
+					},
+				},
+				Uuid: "03b8ffd6-dbef-4745-87a0-46b4f8459e1e",
+			},
+		},
+	}
+
+	t.Parallel()
+	for i := range testcases {
+		tcase := &testcases[i]
+		t.Run(reflect.TypeOf(tcase.expected).Name(), func(t *testing.T) {
+			err := json.NewDecoder(strings.NewReader(tcase.response)).Decode(tcase.actual)
+			if !assert.NoError(t, err) {
+				t.FailNow()
+			}
+
+			assert.Equal(t, tcase.expected, tcase.actual)
+		})
+	}
+}